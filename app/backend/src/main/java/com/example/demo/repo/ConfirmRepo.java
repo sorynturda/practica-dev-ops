a54adb93542d696b0e41d617f9f9233a7750b4f6
Sorin Turda <s.turda12@gmail.com>
2024-08-10 23:42:43 +0300

    aplicatia este in alt repo

--- app/backend/src/main/java/com/example/demo/repo/ConfirmRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.model.MatchConfirm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfirmRepo extends JpaRepository<MatchConfirm,String> {
-}
